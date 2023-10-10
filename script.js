81d4d9b5882ec77a4a2593bfe0522f3705031cd7
tugwellchristi <tugwellchristi@gmail.com>
2023-10-10 16:25:48 -0700

    got checkboxes working! added comments to code

--- script.js
+++ script.js
@@ -1,64 +1,65 @@
 
 // DOM elements
-const result = document.getElementById("password");
-const upperCaseEl = document.querySelector("uppercase");
-const lowerCaseEl = document.querySelector("lowercase");
-const numberEl = document.querySelector("number");
-const symbolEl = document.querySelector("symbol");
-const lengthEl = document.querySelector("length");
+const result = document.querySelector("#password");
+const upperCaseEl = document.querySelector("#uppercase");
+const lowerCaseEl = document.querySelector("#lowercase");
+const numberEl = document.querySelector("#number");
+const symbolEl = document.querySelector("#symbol");
+const lengthEl = document.querySelector("#length");
 const generateBtn = document.getElementById("generate");
-const checkbox = document.getElementById("checkbox");
+
 
 // Global string variables
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const number = "0123456789";
 const symbol = "~!@#$%^&*";
-const length = 8;
 
-// Variable containing all string variables
-const allChars = upperCase + lowerCase + number + symbol;
 
-// Functions used
+// Function used to generate the password
 function writePassword() {
   let password = " ";
+  let length = lengthEl.value;
+  let chars = " "
 
-  // Generates random selections based on the above strings
-  password += upperCase[Math.floor(Math.random() * upperCase.length)];
-  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-  password += number[Math.floor(Math.random() * number.length)];
-  password += symbol[Math.floor(Math.random() * symbol.length)];
-  
-  while (length > password.length) {
-    password += allChars[Math.floor(Math.random() * allChars.length)];
+  // If an element is checked, it pulls from its named string of characters. If not checked, it does not display any characters
+  if (upperCaseEl.checked) {
+    chars += upperCase;
+  } else {
+    chars += " ";
+  }
+
+  if (lowerCaseEl.checked) {
+    chars += lowerCase;
+  } else {
+    chars += " ";
+  }
+
+  if (numberEl.checked) {
+    chars += number;
+  } else {
+    chars += " ";
+  }
+
+  if (symbolEl.checked) {
+    chars += symbol;
+  } else {
+    chars += " ";
+  }
+
+  // For loop increments based on desired length and math random helps generate a number, math floor rounding the decimal place down
+  // This will then identify which number in the character strings that will display in the result window based on the users selection criteria
+  for (let i = 0; i <= length; i++) {
+    let rand = Math.floor(Math.random() * chars.length);
+    password += chars[rand];
   }
   result.value = password;
 }
 
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); 
+// Added event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
 
-// // Get the checkbox element
-// const checkbox = document.getElementById("myCheckbox");
-
-// // Get the string content
-// const stringContent = "This is the content to be added.";
-
-// // Attach a click event listener to the checkbox
-// checkbox.addEventListener("click", function() {
-//   // Get the element where you want to insert the content
-//   const targetElement = document.getElementById("targetElement");
-
-//   // Check if the checkbox is checked
-//   if (checkbox.checked) {
-//     // Insert the content into the desired location in the DOM
-//     targetElement.textContent = stringContent;
-//   } else {
-//     // Clear the content if the checkbox is unchecked
-//     targetElement.textContent = "";
-//   }
-// });
 

a1dded1e7ba21a09959248f47adb4be8e251dbc3
tugwellchristi <90078824+tugwellchristi@users.noreply.github.com>
2024-01-30 09:54:40 -0800

    Update script.js
    -cleaned up conditional statement and included prompts

--- script.js
+++ script.js
@@ -8,58 +8,37 @@ const symbolEl = document.querySelector("#symbol");
 const lengthEl = document.querySelector("#length");
 const generateBtn = document.getElementById("generate");
 
-
 // Global string variables
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const number = "0123456789";
 const symbol = "~!@#$%^&*";
 
-
 // Function used to generate the password
 function writePassword() {
-  let password = " ";
-  let length = lengthEl.value;
-  let chars = " "
+  let password = "";
+  let length = parseInt(lengthEl.value);
+  let chars = "";
 
   // If an element is checked, it pulls from its named string of characters. If not checked, it does not display any characters
-  if (upperCaseEl.checked) {
-    chars += upperCase;
-  } else {
-    chars += " ";
-  }
-
-  if (lowerCaseEl.checked) {
-    chars += lowerCase;
-  } else {
-    chars += " ";
-  }
-
-  if (numberEl.checked) {
-    chars += number;
-  } else {
-    chars += " ";
-  }
-
-  if (symbolEl.checked) {
-    chars += symbol;
-  } else {
-    chars += " ";
-  }
-
+  if (upperCaseEl.checked) chars += upperCase;
+  if (lowerCaseEl.checked) chars += lowerCase;
+  if (numberEl.checked) chars += number;
+  if (symbolEl.checked) chars += symbol;
+ 
   // Alert will be issued if the user does not select any of the checkboxes
-  if (!document.querySelector('input[type="checkbox"]:checked')) {
-    alert("Please select at least one of the following criteria to create your unique password!")
+  if (!chars || length < 8 || length > 128) {
+    alert("Please select at least one of the following criteria to create your unique password!");
+    return;
   }
   // For loop increments based on desired length and math random helps generate a number, math floor rounding the decimal place down
   // This will then identify which number in the character strings that will display in the result window based on the users selection criteria
-  for (let i = 0; i <= length; i++) {
+  for (let i = 0; i < length; i++) {
     let rand = Math.floor(Math.random() * chars.length);
     password += chars[rand];
   }
   result.value = password;
 }
-
 
 // Added event listener to generate button
 generateBtn.addEventListener("click", writePassword);
